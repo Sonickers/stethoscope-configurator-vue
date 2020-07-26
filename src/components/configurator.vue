e3707aa03989e9c1cfd660b7c07ab633e3dc0465
Sonickers <soniarepec@gmail.com>
2020-07-26 16:17:31 +0200

    Move form state up to configurator

--- src/components/configurator.vue
+++ src/components/configurator.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="container has-background-light py-4 px-4">
-        <header-title/>
-        <div class="columns">
-            <div class="column is-two-thirds">
-                <forms/>
-            </div>
-            <div class="column">
-                <result-product/>
-            </div>
-        </div>
+  <div class="container has-background-light py-4 px-4">
+    <header-title />
+    <div class="columns">
+      <div class="column is-two-thirds">
+        <forms v-model="form" />
+      </div>
+      <div class="column">
+        <result-product />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import headerTitle from './header-title'
-import forms from './forms'
-import resultProduct from './result-product'
+import headerTitle from "./header-title";
+import forms from "./forms";
+import resultProduct from "./result-product";
 
 export default {
-    components: {
-        headerTitle,
-        forms,
-        resultProduct
-  }
-}
+  components: {
+    headerTitle,
+    forms,
+    resultProduct,
+  },
+  data() {
+    return {
+      form: {},
+    };
+  },
+};
 </script>
--- src/components/forms.vue
+++ src/components/forms.vue
@@ -1,58 +1,61 @@
 <template>
-    <div class="pt-2">
-        <brand :brands="brands" :active="activeBrand" @selected="selectActiveBrand" />
-        <model :models="models" :active="activeModel" @selected="selectActiveModel" />
-        <color :colors="colors" :active="activeColor" @selected="selectActiveColor" />
-    </div>
+  <div class="pt-2">
+    <brand :brands="brands" :active="value.brand" @selected="selectActiveBrand" />
+    <model :models="models" :active="value.model" @selected="selectActiveModel" />
+    <color :colors="colors" :active="value.color" @selected="selectActiveColor" />
+  </div>
 </template>
 
 <script>
-import brand from './forms/brand'
-import model from './forms/model'
-import color from './forms/color'
+import brand from "./forms/brand";
+import model from "./forms/model";
+import color from "./forms/color";
 
 export default {
-    components: {
-        brand,
-        model,
-        color
+  props: {
+    value: {
+      type: Object,
+      default: () => ({}),
     },
-    data() {
-        return {
-            activeBrand: null,
-            brands: [
-                { id: 1, name: "Littmann", cost: 150 },
-                { id: 2, name: "Spirit Medical", cost: 130 },
-                { id: 3, name: "NaturFarm", cost: 95 },
-                { id: 4, name: "Luxamed", cost: 75 }
-            ],
-            activeModel: null,
-            models: [
-                { id: 1, name: "Intern", cost: 40 },
-                { id: 2, name: "Cardio", cost: 60 },
-                { id: 3, name: "Pediatric", cost: 30 },
-                { id: 4, name: "Anesthesia", cost: 25 }
-            ],
-            activeColor: null,
-            colors: [
-                { id: 1, name: "Summer Sun", color: "is-warning" },
-                { id: 2, name: "Neon Pink", color: "is-danger" },
-                { id: 3, name: "Blueberry Blue", color: "is-link" },
-                { id: 4, name: "Deep Black", color: "is-black" },
-                { id: 5, name: "Cactus Green", color: "is-success" }
-            ]
-        }
+  },
+  components: {
+    brand,
+    model,
+    color,
+  },
+  data() {
+    return {
+      brands: [
+        { id: 1, name: "Littmann", cost: 150 },
+        { id: 2, name: "Spirit Medical", cost: 130 },
+        { id: 3, name: "NaturFarm", cost: 95 },
+        { id: 4, name: "Luxamed", cost: 75 },
+      ],
+      models: [
+        { id: 1, name: "Intern", cost: 40 },
+        { id: 2, name: "Cardio", cost: 60 },
+        { id: 3, name: "Pediatric", cost: 30 },
+        { id: 4, name: "Anesthesia", cost: 25 },
+      ],
+      colors: [
+        { id: 1, name: "Summer Sun", color: "is-warning" },
+        { id: 2, name: "Neon Pink", color: "is-danger" },
+        { id: 3, name: "Blueberry Blue", color: "is-link" },
+        { id: 4, name: "Deep Black", color: "is-black" },
+        { id: 5, name: "Cactus Green", color: "is-success" },
+      ],
+    };
+  },
+  methods: {
+    selectActiveBrand(brand) {
+      this.$emit('input', { ...this.value, brand })
     },
-    methods: {
-        selectActiveBrand(brand) {
-            this.activeBrand =  brand
-        },
-        selectActiveModel(model) {
-            this.activeModel =  model
-        },
-        selectActiveColor(color) {
-            this.activeColor =  color
-        }
-    }
-}
+    selectActiveModel(model) {
+      this.$emit('input', { ...this.value, model })
+    },
+    selectActiveColor(color) {
+      this.$emit('input', { ...this.value, color })
+    },
+  },
+};
 </script>
