974c9a151b9181e9990a4f9916878df21c4e3cf1
Sonickers <soniarepec@gmail.com>
2020-07-28 22:19:08 +0200

    Add png stethoscope and new colors

--- src/components/forms.vue
+++ src/components/forms.vue
@@ -3,6 +3,7 @@
     <brand :brands="brands" :active="value.brand" @selected="selectActiveBrand" />
     <model :models="models" :active="value.model" @selected="selectActiveModel" />
     <color :colors="colors" :active="value.color" @selected="selectActiveColor" />
+    <personalization/>
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import brand from "./forms/brand";
 import model from "./forms/model";
 import color from "./forms/color";
+import personalization from './forms/personalization'
 
 export default {
   props: {
@@ -22,27 +24,29 @@ export default {
     brand,
     model,
     color,
+    personalization
   },
   data() {
     return {
       brands: [
-        { id: 1, name: "Littmann", cost: 150 },
-        { id: 2, name: "Spirit Medical", cost: 130 },
-        { id: 3, name: "NaturFarm", cost: 95 },
-        { id: 4, name: "Luxamed", cost: 75 },
+        { id: 1, name: "Littmann", cost: 160 },
+        { id: 2, name: "Spirit Medical", cost: 140 },
+        { id: 3, name: "NaturFarm", cost: 105 },
+        { id: 4, name: "Luxamed", cost: 105 },
       ],
       models: [
-        { id: 1, name: "Intern", cost: 40 },
-        { id: 2, name: "Cardio", cost: 60 },
-        { id: 3, name: "Pediatric", cost: 30 },
-        { id: 4, name: "Anesthesia", cost: 25 },
+        { id: 1, name: "Intern", cost: 60 },
+        { id: 2, name: "Cardio", cost: 90 },
+        { id: 3, name: "Pediatric", cost: 40 },
+        { id: 4, name: "Anesthesia", cost: 45 },
       ],
       colors: [
-        { id: 1, name: "Summer Sun", color: "is-warning", img: "yellow.jpeg", hex: "#F3C947" },
-        { id: 2, name: "Neon Pink", color: "is-danger", img: "pink.jpg", hex: "#F61974" },
-        { id: 3, name: "Blueberry Blue", color: "is-link", img: "blue.jpg", hex: "#326CB5" },
-        { id: 4, name: "Deep Black", color: "is-black", img: "black.jpg", hex: "#1D2023" },
-        { id: 5, name: "Cactus Green", color: "is-success", img: "green.jpg", hex: "#356865" },
+        { id: 1, name: "Yellow", color: "is-warning", img: "yellow.png", hex: "#F3C947" },
+        { id: 2, name: "Pink", color: "is-danger", img: "pink.png", hex: "#F61974" },
+        { id: 3, name: "Blue", color: "is-link", img: "blue.png", hex: "#326CB5" },
+        { id: 4, name: "Black", color: "is-black", img: "black.png", hex: "#1D2023" },
+        { id: 5, name: "Green", color: "is-success", img: "green.png", hex: "#356865" },
+        { id: 6, name: "Red", color: "is-danger", img: "red.png", hex: "#D6231E" },
       ],
     };
   },
