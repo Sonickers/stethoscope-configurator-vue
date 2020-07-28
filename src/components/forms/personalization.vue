5bb4727ed6b3f0643391c41433921c40753ae4fb
Sonickers <soniarepec@gmail.com>
2020-07-28 22:19:41 +0200

    Add personalization input

---
+++ src/components/forms/personalization.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2 class="pb-2">Personalization</h2>
+    <div class="control">
+      <div class="field">
+        <div class="control">
+          <input class="input is-primary is-rounded" type="text" placeholder="Primary input" />
+        </div>
+      </div>
+      <hr />
+    </div>
+  </div>
+</template>
